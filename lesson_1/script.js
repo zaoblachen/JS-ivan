9dee8b67df1e61bbd3d53b294310197929cf1998
JS-ivan <zaoblachen@gmail.com>
2020-10-30 19:15:52 +0300

    Это наш первый комит

---
+++ lesson_1/script.js
@@ -0,0 +1,100 @@
+"use strict";
+
+a = 5;
+console.log(a);
+
+var number = 5;
+var string = "Hello!";
+var sym = Symbol();
+var boolean = true;
+null; // не существует
+undefined; // значения нет
+var obj = {};
+
+console.log(4 / 0);
+console.log('string' * 4);
+
+let something;
+console.log(something);
+
+let person = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(person.name);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+console.log(arr[0]);
+
+//alert("Hello!");
+
+//let answer = confirm("Are u sure?");
+
+//console.log(answer);
+
+//let answer = +prompt("Есть ли Вам 18?", "Да");
+
+//console.log(typeof(answer));
+
+//console.log("art" + " - object");
+
+//console.log(4 + +" - object");
+
+
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log("2" === 2);
+
+let isChecked = true,
+    isClose = true,
+    isClyu = true;
+
+console.log(!isChecked || isClose);
+
+
+
+
+
+//console.log(typeof(String(false+"")));
+
+//console.log("ww" + 5);
+//console.log('https://vk.com/catalog/' + 5);
+
+////to Number
+
+//console.log(typeof(Number("5")));
+
+//console.log(typeof(5 + '5'));
+
+//console.log(typeof(parseInt('15px', 10)));
+
+//let ans = +prompt("Hello?", '');
+
+//// 0, '', null, undefined, Nan
+
+//let switcher = null;
+
+//if (switcher) {
+//   console.log("working..");
+//}
+
+//switcher = 1;
+
+//if (switcher) {
+//   console.log("working..");
+//}
+
+
+//// 2)
+//console.log(typeof(Boolean("5")));
+
+//// 3)
+//console.log(typeof(!!"5"));
