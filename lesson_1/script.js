e67038a34b988f3186761be1ca30323108d3985e
JS-ivan <zaoblachen@gmail.com>
2020-11-03 18:34:39 +0300

    домашка № 3

--- lesson_1/script.js
+++ lesson_1/script.js
@@ -1,100 +1,124 @@
-"use strict";
+//"use strict";
 
-a = 5;
-console.log(a);
+//a = 5;
+//console.log(a);
 
-var number = 5;
-var string = "Hello!";
-var sym = Symbol();
-var boolean = true;
-null; // не существует
-undefined; // значения нет
-var obj = {};
+//var number = 5;
+//var string = "Hello!";
+//var sym = Symbol();
+//var boolean = true;
+//null; // не существует
+//undefined; // значения нет
+//var obj = {};
 
-console.log(4 / 0);
-console.log('string' * 4);
+//console.log(4 / 0);
+//console.log('string' * 4);
 
-let something;
-console.log(something);
+//let something;
+//console.log(something);
 
-let person = {
-    name: "John",
-    age: 25,
-    isMarried: false
-};
+//let person = {
+//    name: "John",
+//    age: 25,
+//    isMarried: false
+//};
 
-console.log(person.name);
+//console.log(person.name);
 
-let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+//let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
 
-console.log(arr[0]);
+//console.log(arr[0]);
 
-//alert("Hello!");
+////alert("Hello!");
 
-//let answer = confirm("Are u sure?");
+////let answer = confirm("Are u sure?");
 
-//console.log(answer);
+////console.log(answer);
 
-//let answer = +prompt("Есть ли Вам 18?", "Да");
+////let answer = +prompt("Есть ли Вам 18?", "Да");
 
-//console.log(typeof(answer));
+////console.log(typeof(answer));
 
-//console.log("art" + " - object");
+////console.log("art" + " - object");
 
-//console.log(4 + +" - object");
-
-
-
-let incr = 10,
-    decr = 10;
-
-console.log(incr++);
-console.log(decr--);
-
-console.log("2" === 2);
-
-let isChecked = true,
-    isClose = true,
-    isClyu = true;
-
-console.log(!isChecked || isClose);
+////console.log(4 + +" - object");
 
 
 
+//let incr = 10,
+//    decr = 10;
+
+//console.log(incr++);
+//console.log(decr--);
+
+//console.log("2" === 2);
+
+//let isChecked = true,
+//    isClose = true,
+//    isClyu = true;
+
+//console.log(!isChecked || isClose);
 
 
-//console.log(typeof(String(false+"")));
 
-//console.log("ww" + 5);
-//console.log('https://vk.com/catalog/' + 5);
 
-////to Number
 
-//console.log(typeof(Number("5")));
+////console.log(typeof(String(false+"")));
 
-//console.log(typeof(5 + '5'));
+////console.log("ww" + 5);
+////console.log('https://vk.com/catalog/' + 5);
 
-//console.log(typeof(parseInt('15px', 10)));
+//////to Number
 
-//let ans = +prompt("Hello?", '');
+////console.log(typeof(Number("5")));
 
-//// 0, '', null, undefined, Nan
+////console.log(typeof(5 + '5'));
 
-//let switcher = null;
+////console.log(typeof(parseInt('15px', 10)));
 
-//if (switcher) {
-//   console.log("working..");
+////let ans = +prompt("Hello?", '');
+
+////// 0, '', null, undefined, Nan
+
+////let switcher = null;
+
+////if (switcher) {
+////   console.log("working..");
+////}
+
+////switcher = 1;
+
+////if (switcher) {
+////   console.log("working..");
+////}
+
+
+////// 2)
+////console.log(typeof(Boolean("5")));
+
+////// 3)
+////console.log(typeof(!!"5"));
+
+////let num = 50;
+
+//////while (num < 55) {
+//////    console.log(num);
+//////    num++
+//////}
+
+////do {
+////    console.log(num);
+////    num++
+////}
+
+////while (num < 55)
+
+//for (let i = 1; i < 8; i++) {
+//    if (i == 6) {
+//        continue;
+//    }
+//    console.log(i);
 //}
 
-//switcher = 1;
 
-//if (switcher) {
-//   console.log("working..");
-//}
-
-
-//// 2)
-//console.log(typeof(Boolean("5")));
-
-//// 3)
-//console.log(typeof(!!"5"));
+console.log(0 || "" || undefined || falsе);
--- lesson_1hw/script.js
+++ lesson_1hw/script.js
@@ -1,24 +1,42 @@
-'use strict';
+let money = +prompt("Ваш бюджет на месяц?");
+time = prompt("Введите дату", "YYYY-MM-DD");
 
-
-var money = prompt("Ваш бюджет на месяц?");
-var time = prompt("Введите дату", "YYYY-MM-DD");
-
-var appData = {
-    moneyData: money,
+let appData = {
+    budjet: money,
     timeData: time,
     expenses: {},
-    optionalExpenses: "",
+    optionalExpenses: {},
     income: [],
     savings: false
+};
+
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", "");
+    b = prompt("Во сколько обойдется?", "");
+
+    if (typeof (a) === 'string' &&
+        (typeof (a) != null &&
+            (typeof (b) != null &&
+                a != '' &&
+                b != '' &&
+                a.length < 50))) {
+        console.log("done");
+        appData.expenses[a] = b;
+    } else {
+        //вопрос заново
+    }
 }
 
-let expenses = new Object;
+appData.moneyPerDay = appData.budjet / 30;
 
-expenses.st1 = prompt("Введите обязательную статью расходов в этом месяце");
-expenses.cash1 = prompt("Во сколько обойдется?");
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
 
-expenses.st2 = prompt("Введите обязательную статью расходов в этом месяце");
-expenses.cash2 = prompt("Во сколько обойдется?");
-
-alert(Number(expenses.cash1) + Number(expenses.cash2));
+if (appData.moneyPerDay < 100) {
+    console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний уровен достатка");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+} else {
+    console.log("Произошла ошибка");
+}
